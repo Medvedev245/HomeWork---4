e3fda02da03f3ea7082c29367abf1ead95631a10
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-12-23 13:08:48 +0100

    add pass. Add mongodb. add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,84 @@
-import app from "./app.js";
+//           77g6DThdx1oEOo77   -----  pasword mongo
+//  mongodb+srv://Ihor:77g6DThdx1oEOo77@cluster0.fmo3gi2.mongodb.net/my-contacts?retryWrites=true&w=majority
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+import app from "./app.js";
+const DB_HOST =
+  "mongodb+srv://Ihor:77g6DThdx1oEOo77@cluster0.fmo3gi2.mongodb.net/my-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+/*
+[
+  {
+    "id": "AeHIrLTr6JkxGE6SN-0Rw",
+    "name": "Allen Raymond",
+    "email": "nulla.ante@vestibul.co.uk",
+    "phone": "(992) 914-3792"
+  },
+  {
+    "id": "qdggE76Jtbfd9eWJHrssH",
+    "name": "Chaim Lewis",
+    "email": "dui.in@egetlacus.ca",
+    "phone": "(294) 840-6685"
+  },
+  {
+    "id": "drsAJ4SHPYqZeG-83QTVW",
+    "name": "Kennedy Lane",
+    "email": "mattis.Cras@nonenimMauris.net",
+    "phone": "(542) 451-7038"
+  },
+  {
+    "id": "vza2RIzNGIwutCVCs4mCL",
+    "name": "Wylie Pope",
+    "email": "est@utquamvel.net",
+    "phone": "(692) 802-2949"
+  },
+  {
+    "id": "05olLMgyVQdWRwgKfg5J6",
+    "name": "Cyrus Jackson",
+    "email": "nibh@semsempererat.com",
+    "phone": "(501) 472-5218"
+  },
+  {
+    "id": "1DEXoP8AuCGYc1YgoQ6hw",
+    "name": "Abbot Franks",
+    "email": "scelerisque@magnis.org",
+    "phone": "(186) 568-3720"
+  },
+  {
+    "id": "Z5sbDlS7pCzNsnAHLtDJd",
+    "name": "Reuben Henry",
+    "email": "pharetra.ut@dictum.co.uk",
+    "phone": "(715) 598-5792"
+  },
+  {
+    "id": "C9sjBfCo4UJCWjzBnOtxl",
+    "name": "Simon Morton",
+    "email": "dui.Fusce.diam@Donec.com",
+    "phone": "(233) 738-2360"
+  },
+  {
+    "id": "e6ywwRe4jcqxXfCZOj_1e",
+    "name": "Thomas Lucas",
+    "email": "nec@Nulla.com",
+    "phone": "(704) 398-7993"
+  },
+  {
+    "id": "tefK9ULGJOSZwMmYBc1dV",
+    "name": " 8989",
+    "email": "nec@Nulla.com",
+    "phone": "5345345345345"
+  }
+]
+*/
